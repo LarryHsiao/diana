c6cbad19dc7f14ed3630470598a3f55137458e1a
Larry Hsiao <larryhsiao@silverhetch.com>
2019-01-18 02:41:26 +0800

    remove unused use

--- src/sqlite_tagging/mod.rs
+++ src/sqlite_tagging/mod.rs
@@ -1,12 +1,3 @@
 pub mod conn;
 pub mod objects;
 pub mod object;
-
-use crate::tagging::Object;
-use crate::tagging::Objects;
-
-use sqlite::Connection;
-use sqlite::Statement;
-use sqlite::State;
-
-
